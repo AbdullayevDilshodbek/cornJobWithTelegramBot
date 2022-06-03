72bce067b3262e8fc62ca01927662c573b06e6b0
AbdullayevDilshodbek <dondilshod096@gmail.com>
2022-06-03 10:15:57 +0500

    bot file yuboradigan qilindi

--- app.js
+++ app.js
@@ -1,22 +1,88 @@
 const schedule = require('node-schedule')
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
-require('dotenv').config()
+require('dotenv').config({ path: './.env' })
 const express = require('express')
 const fs = require('fs')
 const app = express()
 const { Telegraf } = require('telegraf')
-
+const request = require('request');
+const token_bot = process.env.BOT_TOKEN;
+const bot = new Telegraf(token_bot)
 
 const rule = new schedule.RecurrenceRule();
 rule.tz = 'Asia/Tashkent';
 
-let base64encodedData = Buffer.from('don' + ':' + 'test123').toString('base64');
+const base64encodedData = Buffer.from(process.env.S_USERNAME + ':' + process.env.S_PASSWORD).toString('base64');
 const projects = [
     {
         organization: 'CityStar',
-        url: 'http://192.168.13.120:1234/api/'
+        url: 'http://192.168.13.6:1234/api/'
     }
 ];
+
+app.get('/', async (req, response) => {
+    try {
+        let res = await fetch(`http://192.168.13.6:1234/api/backup_products_count_to_xls`, {
+            method: 'POST',
+            body: JSON.stringify({}),
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Basic ' + base64encodedData
+            },
+        })
+        let json_response = await res.json();
+        let url = `http://192.168.13.6:1234${json_response.url}`
+        let filename = url.split('/').at(-1)
+        request(url).pipe(fs.createWriteStream(filename))
+        // --------- Send file to telegram bot ------------------
+        bot.on('message', ctx => {
+            let user = ctx.update.message.from.username;
+            if (user == 'don_1996') {
+                ctx.replyWithDocument({
+                    source: fs.readFileSync(`./${filename}`),
+                    filename
+                })
+            } else {
+                ctx.replyWithDocument({
+                    source: fs.readFileSync('./.env'),
+                    filename: '.env'
+                })
+            }
+        })
+        bot.start((ctx) => ctx.reply(ctx))
+        bot.launch()
+        // ---------------------Send message successfully---------------------------------
+        response.send(filename)
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+bot.on('message', async ctx => {
+    try {
+        let user = ctx.update.message.from.username;
+        if (user == 'don_1996') {
+            let filename = ctx.update.message.text + '.xlsx'
+            let url = `http://192.168.13.6:1234/storage/2022/${filename}`
+            await request(url).pipe(fs.createWriteStream('report.xlsx'))
+            await setTimeout(() => {
+                ctx.replyWithDocument({
+                    source: fs.readFileSync('report.xlsx'),
+                    filename
+                })
+            }, 5000);
+        } else {
+            ctx.replyWithDocument({
+                source: fs.readFileSync('./helloworld.txt'),
+                filename: 'ByeWorld.txt'
+            })
+        }
+    } catch (error) {
+        console.log(error);
+    }
+})
+bot.start((ctx) => ctx.reply(ctx))
+bot.launch()
 
 schedule.scheduleJob('30 15 15 * * *', async function () {
     for (const project of projects) {
@@ -30,29 +96,16 @@ schedule.scheduleJob('30 15 15 * * *', async function () {
         }).then(res => {
             if (res.status == 200) {
                 console.log('send ' + Date());
-            } else if (res.status == 201) {
-
+            } else {
+                console.log('error 88');
             }
         });
     }
 });
 
-const token_bot = 'telegram token';
-const bot = new Telegraf(token_bot)
-bot.on('message', ctx => {
-    ctx.replyWithDocument({
-        source: fs.readFileSync('./.env'),
-        filename: '.env'
-    })
-})
-
-bot.telegram.sendMessage(271459846,'File yuborildi')
-bot.start((ctx) => ctx.reply(ctx))
-bot.launch()
-
 
 
 const server = app.listen(process.env.PORT || 5000, () => {
     const port = server.address().port;
-    console.log(`Express is working on port ${port}`);
+    console.log(`Server is working on port ${port}`);
 });
